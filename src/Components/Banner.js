f2f86e0f0ab764ed3cebc1a69d6a2624d75933c9
Brendan Piper <BLPiper01@gmail.com>
2025-09-04 13:15:07 -0600

    Added linear-gradient to the Banner

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,17 +1,16 @@
-import Box from '@mui/material/Box';
-import { Typography, Button, ButtonGroup } from '@mui/material';
+import { Box, Typography, Button, ButtonGroup, Link } from '@mui/material';
 
 function Banner({ title }) {
     return (
         <Box sx={{
             width: '100%',
             height: '25%',
-            backgroundColor: 'orange'
+            background: 'linear-gradient(90deg,rgba(235, 90, 0, 1) 0%, rgba(255, 255, 255, 1) 65%, rgba(235, 90, 0, 1) 100%)'
         }}>
             <Box sx={{
-                backgroundColor: 'white',
                 width: '100px',
-                height: '100px'
+                height: '100px',
+                backgroundColor: 'white'
             }}>
                 <Typography>
                     Bremdfam's Website
@@ -21,10 +20,14 @@ function Banner({ title }) {
                 </Typography>
                 <ButtonGroup>
                     <Button>
+                        <Link href='#'>
                         Projects
+                        </Link>
                     </Button>
                     <Button>
+                        <Link href='https://github.com/Bremdfam' target='_blank'>
                         Github
+                        </Link>
                     </Button>
                 </ButtonGroup>
             </Box>
